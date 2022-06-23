466bc4d8b695312ac5de3a01fd520f3b0fca14a2
Ida <ida.segond.von.banchet@ing.com>
2022-06-23 09:32:42 +0200

    Initial Commit

---
+++ PhonyBankingApp/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  PhonyBankingApp
+//
+//  Created by Segond von Banchet, I.G.C. (Ida Gerri Christina) on 23/06/2022.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
